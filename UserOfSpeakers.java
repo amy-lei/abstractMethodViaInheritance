845e91601c32a792961fbd392480b4913adc8b9b
god <ksacks00@stuy.edu>
2019-03-01 13:57:26 -0500

    UserOfSpeakers.java

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -1,4 +1,4 @@
-public class UserOfSpeakers{ //I helped
+public class UserOfSpeakers{ //hi
    public static void main(String[] arg){
 	Animal clifford = new Retriever();
 	System.out.println( clifford.speak());
